b2240b970fc0ad4048fab05fb969c8253e4a0f44
ankit <ankit.sanghavi87@gmail.com>
2021-10-07 14:08:28 +0530

    gh setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { createConnection } from 'typeorm'
+
+const main = async () => {
+    try {
+        await createConnection({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'anA56sz3*P112',
+            database:'typeorm'
+        })
+        console.log('connected to postgres')
+    } catch (error) {
+        console.log('error connecting to postgres');
+    }
+}
+
+main();
